52b217b9d59bc6b91b165de5290c98a944c78f17
Hooman Vaseli <Hooman Vaseli>
2016-04-16 23:21:48 -0700

    codes used in arduinos

---
+++ Amber_Light_New_Library/Lights_Control_front_With_Horn.ino
@@ -0,0 +1,237 @@
+
+
+/*
+ *  This code is for receiving messages regarding indicator lights and 
+ *  taking action accordingly.
+ *  
+ *  We have 7 lights on the car
+ *  2 front, 2 front sides, 2 back, and 1 back center up
+ *  
+ *  We need 5 outputs to control all the lights
+ *  
+ *  1 for front and side, both in the right side
+ *  1 for front and side, both in the left side
+ *  1 for back left
+ *  1 for back right
+ *  1 for back center
+ *  
+ *  ID(dec) SYSTEM              LENGTH  FORMAT
+ *  0        brake                1     data[0] = brake status (0=OFF, 1=ON)
+ *  
+ *  1       Hazard                1     data[0] = hazard status(0=OFF, 1=ON)
+ *  
+ *  9       signals               1     data[0].0 = left turning signal status (0=OFF, 1=ON)
+ *                                      data[0].1 = right turning signal status (0=OFF, 1=ON)
+ *  
+ * 
+ */
+
+ #include <SPI.h>
+#include <mcp_can.h>
+#include <ubcsolar_can_ids.h>
+
+// SPI_CS_PIN should be 10 for our older version of shield. It is 9 for the newer version.
+const int SPI_CS_PIN = 9;
+MCP_CAN CAN(SPI_CS_PIN); 
+
+#define TRUE 1
+#define FALSE 0
+
+
+#define HORN1 4 
+#define HORN2 5
+#define CAN_ID_HORN 7
+
+// 2 groups of outputs for 4 lights, output pins can be changed according to pins available
+#define FRONT_SIDE_R_PIN   3
+#define FRONT_SIDE_L_PIN   2
+
+// 4 flages which are actually the message recieved
+boolean Hazard=0;
+boolean Left_Sig=0;
+boolean Right_Sig=0;
+
+/*used to determine if the LED should blink or not. (value determined according to the 4 flags)
+ *ledBLINK = 0 => NOT BLINK  ledBLINK = 1 => BLINK
+ */
+boolean ledBLINK_R=0;        // right side lights
+boolean ledBLINK_L=0;        // left side lights
+boolean ledBLINK_ALL=0;      // all 4 lights
+
+// LED states for the lights. 5 outputs
+int ledState_FR =LOW;
+int ledState_FL =LOW;
+
+// time intervals used for blinking
+#define HAZARD_INTERVAL 125
+#define NORMAL_INTERVAL 300
+
+unsigned long previousMillis =0;
+long interval = NORMAL_INTERVAL;       
+
+void setup() {
+  
+// SERIAL INIT 
+    Serial.begin(115200);
+
+// CAN INIT 
+    int canSSOffset = 0;
+    
+    pinMode(FRONT_SIDE_R_PIN,OUTPUT);
+    pinMode(FRONT_SIDE_L_PIN,OUTPUT);
+
+    pinMode(HORN1, OUTPUT);
+    pinMode(HORN2, OUTPUT);
+
+START_INIT:
+
+    if(CAN_OK == CAN.begin(CAN_125KBPS))                   // init can bus : baudrate = 125k
+    {
+        Serial.println("CAN BUS Shield init ok!");
+    }
+    else
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.print("Init CAN BUS Shield again with SS pin ");
+        Serial.println(SPI_CS_PIN + canSSOffset);
+        
+        delay(100);
+
+        canSSOffset ^= 1;
+        CAN = MCP_CAN(SPI_CS_PIN + canSSOffset);
+        goto START_INIT;
+    }
+
+}
+
+void loop() {
+  
+    unsigned char len=0, buf[8], canID;
+    
+    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data is coming
+    {
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        canID = CAN.getCanId(); 
+        
+        Serial.println("------------------------------------------");
+        Serial.print("get data from ID: ");
+        Serial.println(canID);
+
+        Serial.print("Frame Data:  ");
+        for(int i = 0; i<len; i++)    // print the data
+        {
+            Serial.print(buf[i]);
+            Serial.print("\t");
+        }
+
+        
+        if (canID == CAN_ID_HAZARD)  //Emergency Hazard message
+        {
+            if (buf[0] == 1)  //Emergency Hazard ON
+            {
+                Hazard = TRUE;
+                Serial.println("leds should start blinking. Emergency Hazard!!!!" );
+                
+                interval = HAZARD_INTERVAL;   // to make the light blink faster
+            }
+                
+            else if (buf[0] == 0)  //Emergency Hazard OF
+            {
+                Hazard = FALSE;
+                Serial.println("leds should stop blinking. Emergency Hazard is over!!" );
+
+                interval = NORMAL_INTERVAL;   // to make the light blink with normal intervals
+            }
+        }
+        
+        else if (canID == CAN_ID_SIGNAL_CTRL)   // Turning Indicator message
+        {
+            if (buf[0] == 1)  //Turning left side Indicators ON
+            {
+                Left_Sig = TRUE;
+                Serial.println("LEFT side lights should start blinking. Turning Indicator ON" );
+            }
+            else if (buf[0] == 2) //Turning right side Indicators ON
+            {
+                Right_Sig = TRUE;
+                Serial.println("RIGHT side lights should start blinking. Turning Indicator ON" );
+            }                    
+            else if (buf[0] == 0) //Turning Indicators OFF
+            {
+                Right_Sig = FALSE;
+                Left_Sig = FALSE;
+                Serial.println("led should stop blinking. Turning Indicator OFF" );
+            }
+        }
+        else if (canID == CAN_ID_HORN)   //Horn message
+        {             
+
+            if(buf[0] == 0)
+            {
+              Serial.println("Horn is off");
+              digitalWrite(HORN1, LOW);
+              digitalWrite(HORN2, LOW);
+            }
+            
+            else if(buf[0] == 1)
+            {
+              Serial.println("Horn is on");
+              digitalWrite(HORN1, HIGH);
+              digitalWrite(HORN2, HIGH);
+            }
+        }
+        
+    }
+
+    // determining the conditions according to flags
+    ledBLINK_ALL = Hazard;
+    ledBLINK_R = Hazard || Right_Sig;               
+    ledBLINK_L = Hazard || Left_Sig;
+
+// to turn off all the lights which should NOT BLINK or be ON
+    
+    if ( !ledBLINK_R )
+        ledState_FR = LOW;
+        
+    if ( !ledBLINK_L )
+        ledState_FL = LOW;
+
+    
+    unsigned long currentMillis = millis();
+
+    if (ledBLINK_R || ledBLINK_L || ledBLINK_ALL)
+        
+    {           
+            if ( currentMillis - previousMillis >= interval)    // to blink the amber light. instead of using delay();
+            {
+                previousMillis = currentMillis;
+
+                if (ledBLINK_ALL) //hazard
+                {
+                   ledState_FR =!ledState_FR;
+                   Serial.print("Front Right---" );
+                   Serial.print(ledState_FR);
+                   
+                   ledState_FL =ledState_FR;
+                   Serial.print("--Front Left---" );
+                   Serial.print(ledState_FL);
+                }
+                else if(ledBLINK_R)
+                {
+                   ledState_FR =!ledState_FR;
+                   Serial.print("Front Right---" );
+                   Serial.print(ledState_FR);
+                }
+                else if(ledBLINK_L)
+                {                                 
+                   ledState_FL =!ledState_FL;
+                   Serial.print("Front Left---" );
+                   Serial.print(ledState_FL);
+                }
+            }
+
+    }
+    digitalWrite(FRONT_SIDE_R_PIN, ledState_FR);
+    digitalWrite(FRONT_SIDE_L_PIN, ledState_FL);
+}
