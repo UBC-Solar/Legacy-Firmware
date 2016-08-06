1c58362b4f794e5e7422a79aa1fd8f75eb6c0264
jcj83429 <jcj83429@gmail.com>
2016-08-06 15:15:32 -0700

    can log parser no longer crashes at end of file

--- can_log_parse.py
+++ can_log_parse.py
@@ -8,20 +8,23 @@ if len(sys.argv) < 2:
 
 f = open(sys.argv[1], "rb")
 
-while True:
-    year = ord(f.read(1)[0])
-    month = ord(f.read(1)[0])
-    day = ord(f.read(1)[0])
-    hour = ord(f.read(1)[0])
-    minute = ord(f.read(1)[0])
-    second = ord(f.read(1)[0])
+try:
+	while True:
+		year = ord(f.read(1)[0])
+		month = ord(f.read(1)[0])
+		day = ord(f.read(1)[0])
+		hour = ord(f.read(1)[0])
+		minute = ord(f.read(1)[0])
+		second = ord(f.read(1)[0])
 
-    id = 0
-    for i in range(0,4):
-        id += ord(f.read(1)[0]) << (i * 8)
+		id = 0
+		for i in range(0,4):
+			id += ord(f.read(1)[0]) << (i * 8)
 
-    length = ord(f.read(1)[0])
+		length = ord(f.read(1)[0])
 
-    data = f.read(length)
+		data = f.read(length)
 
-    print "[%02d/%02d/%02d %02d:%02d:%02d][%d] %s" % (year, month, day, hour, minute, second, id, " ".join("{:02x}".format(ord(c)) for c in data))
+		print "[%02d/%02d/%02d %02d:%02d:%02d][%d] %s" % (year, month, day, hour, minute, second, id, " ".join("{:02x}".format(ord(c)) for c in data))
+except IndexError: #end of file
+	pass
