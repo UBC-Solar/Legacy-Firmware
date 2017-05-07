b45cc64cd18b5d713004f5e017d81bae3e0b13de
Ezra Savard <git@ezrasavard.com>
2017-05-07 11:40:03 -0700

    can library first pass impl

--- libraries/UBCSolar_CAN_IDs/ubcsolar_can.cpp
+++ libraries/UBCSolar_CAN_IDs/ubcsolar_can.cpp
@@ -1,42 +1,51 @@
 #include "ubcsolar_can.h"
 
-#include <mcp_can.h>
 
-using namespace comm;
-
-uint8_t Heartbeat::getId(const uint8_t* payload) {
-
+uint8_t HeartbeatMessage::getForwardThrottle() {
+  return _buffer[FORWARD_THROTTLE_INDEX];
 }
 
-uint32_t Heartbeat::getPeriod(const uint8_t* payload) {
-
+uint8_t HeartbeatMessage::getRegen() {
+  return _buffer[REVERSE_THROTTLE_INDEX];
 }
 
-uint8_t Heartbeat::getForwardThrottle(const uint8_t* payload) {
-
+HeartbeatMessage::Direction HeartbeatMessage::getThrottleDirection() {
+  return Direction(_buffer[DIRECTION_STATUS_INDEX]);
 }
 
-uint8_t Heartbeat::getRegen(const uint8_t* payload) {
-
+HeartbeatMessage::Signal HeartbeatMessage::getSignals() {
+  return Signal(_buffer[INTERFACE_STATUS_INDEX] & (RIGHT_SIGNAL_MASK | LEFT_SIGNAL_MASK));
 }
 
-Heartbeat::Direction Heartbeat::getThrottleDirection(const uint8_t* payload) {
-
+CommErrorMessage::SystemId CommErrorMessage::getSystemId() {
+  return SystemId(_buffer[SYSTEM_ID_INDEX]);
 }
 
-Heartbeat::Signal Heartbeat::getSignals(const uint8_t* payload) {
-
+uint32_t CommErrorMessage::getTimestamp() {
+  return _buffer[TIMESTAMP_INDEX];
 }
 
-CommError::SystemId getSystemId(const uint8_t* payload) {
+CANDriver::CANDriver(uint8_t pin, uint8_t alternate, uint8_t baudrate)
+  : _pin(pin), _alternate(alternate), _baudrate(baudrate), _can(pin) { }
 
+void CANDriver::begin() {
+  uint8_t activePin = _pin;
+  while (_can.begin(_baudrate) != CAN_OK) {
+    // Alternate between two possible pins until the correct pin is found
+    activePin = (activePin == _pin) ? _alternate : _pin;
+    delay(100);
+    _can = MCP_CAN(activePin);
+  }
+
+  Serial.print("CAN initialized on pin ");
+  Serial.println(activePin);
 }
 
-uint32_t CommError::getTimestamp(const uint8_t* payload) {
+boolean CANDriver::checkMessage(Message& message) {
+  if (_can.checkReceive() != CAN_MSGAVAIL) {
+    return false;
+  }
+  uint8_t result = _can.readMsgBufID(&message._frameId, &message._msgLength, message._buffer);
 
-}
-
-
-MCP_CAN initCAN(uint8_t pin1, uint8_t pin2) {
-
+  return result == CAN_OK;
 }
--- libraries/UBCSolar_CAN_IDs/ubcsolar_can.h
+++ libraries/UBCSolar_CAN_IDs/ubcsolar_can.h
@@ -1,91 +1,128 @@
 #ifndef UBCSOLAR_CAN //include only once
 #define UBCSOLAR_CAN
 
+#include <Arduino.h>
+#include <stdint.h>
+#include <mcp_can.h>
 #include "ubcsolar_can_ids.h"
 
-#include <stdint.h>
+// a generic message
+class Message {
+  public:
+    // Receive buffer fields
+    uint32_t _frameId;
+    uint8_t _msgLength;
+    uint8_t _buffer[8];
+};
 
-class MCP_CAN;
+// all known information about driver interface and status
+class HeartbeatMessage : Message {
+  private:
+    // byte indices in payload
+    static const uint8_t FORWARD_THROTTLE_INDEX = 0;
+    static const uint8_t REVERSE_THROTTLE_INDEX = 1;
+    static const uint8_t DIRECTION_STATUS_INDEX = 2;
+    static const uint8_t INTERFACE_STATUS_INDEX = 3;
+    // bitmasks for status indices
+    static const uint8_t RIGHT_SIGNAL_MASK = 0x1;
+    static const uint8_t LEFT_SIGNAL_MASK = 0x1 << 1;
 
-namespace comm {
+  public:
+    static const uint8_t CAN_ID = CAN_ID_HEARTBEAT;
+    static const uint32_t HEARTBEAT_PERIOD = 100; // milliseconds
 
-  // all known information about driver interface and status
-  class Heartbeat {
-    private:
-      static const uint8_t CAN_ID = CAN_ID_HEARTBEAT;
-      static const uint32_t HEARTBEAT_PERIOD = 100; // milliseconds
-      // byte indices in payload
-      static const uint8_t FORWARD_THROTTLE_INDEX = 0;
-      static const uint8_t REVERSE_THROTTLE_INDEX = 1;
-      static const uint8_t DIRECTION_STATUS_INDEX = 2;
-      static const uint8_t INTERFACE_STATUS_INDEX = 3;
-      // bitmasks for status indices
-      static const uint8_t RIGHT_SIGNAL_MASK = 0x1;
-      static const uint8_t LEFT_SIGNAL_MASK = 0x2;
+    enum class Direction : uint8_t {
+      forward = 0,
+      reverse = 1,
+      neutral = 2,
+    };
 
-    public:
-      enum class Direction : uint8_t {
-        forward = 0,
-        reverse = 1,
-        neutral = 2,
-      };
+    enum class Signal : uint8_t {
+      right = 1,
+      left = 2,
+      hazard = 3,
+    };
 
-      enum class Signal : uint8_t {
-        right = 1,
-        left = 2,
-        hazard = 3,
-      };
+    uint8_t getForwardThrottle();
+    uint8_t getRegen();
+    Direction getThrottleDirection();
+    Signal getSignals();
+};
 
-      static uint8_t getId(const uint8_t* payload);
-      static uint32_t getPeriod(const uint8_t* payload);
-      static uint8_t getForwardThrottle(const uint8_t* payload);
-      static uint8_t getRegen(const uint8_t* payload);
-      static Direction getThrottleDirection(const uint8_t* payload);
-      static Signal getSignals(const uint8_t* payload);
-  };
+// sent by nodes that do not receive heartbeats, for logging
+class CommErrorMessage : Message {
+  private:
+    // byte indices in payload
+    static const uint8_t SYSTEM_ID_INDEX = 0;
+    static const uint8_t TIMESTAMP_INDEX = 1;
 
-  // sent by nodes that do receive heartbeats, for logging
-  class CommError {
-    private:
-      static const uint8_t CAN_ID = CAN_ID_COMM_ERROR;
-      // byte indices in payload
-      static const uint8_t SYSTEM_ID_INDEX = 0;
-      static const uint8_t TIMESTAMP_INDEX = 1;
+  public:
+    static const uint8_t CAN_ID = CAN_ID_COMM_ERROR;
 
-      static const uint8_t TIMESTAMP_BYTES = 7;
+    enum class SystemId : uint8_t {
+      handbrake = 0,
+      commander = 1,
+      motorControl = 2,
+      // TODO more systems
+    };
 
-    public:
-      enum class SystemId : uint8_t {
-        handbrake = 0,
-        commander = 1,
-        motorControl = 2,
-        // more TBD
-      };
+    SystemId getSystemId();
+    uint32_t getTimestamp();
+};
 
-      static SystemId getSystemId(const uint8_t* payload);
-      static uint32_t getTimestamp(const uint8_t* payload);
-  };
+// signals application of the handbrake
+class HandbrakeMessage : Message {
+  public:
+    static const uint8_t CAN_ID = CAN_ID_HANDBRAKE;
 
-  // signals application of the handbrake
-  class Handbrake{
-    private:
-      static const uint8_t CAN_ID = CAN_ID_HANDBRAKE;
+    // TODO
+};
 
-      // TODO
-  };
+// encoder output updates for speed measurement
+class EncoderMessage : Message {
+  public:
+    static const uint8_t CAN_ID = CAN_ID_SPEED;
 
-  // encoder output updates for speed measurement
-  class Speed {
-    private:
-      static const uint8_t CAN_ID = CAN_ID_SPEED;
+    // TODO
 
-      // TODO
+};
 
-  };
+/**
+ * @brief Wrapper for MCP_CAN library.
+ */
+class CANDriver {
+ public:
+  /**
+   * Creates a new CANDriver instance to parse and handle CAN messages.
+   * CANDriver will attempt to read CAN messages from the supplied pin. If
+   * that doesn't work, it will try the alternate pin.
+   * @param pin The Arduino pin.
+   * @param alternate Alternate pin.
+   * @param baudrate The baudrate; see MCP_CAN library for definitions.
+   */
+  CANDriver(uint8_t pin=10, uint8_t alternate=9, uint8_t baudrate=CAN_125KBPS);
 
-  // initialize the CAN shield
-  MCP_CAN initCAN(uint8_t pin1, uint8_t pin2);
+  /**
+   * Reads a CAN message, if there is one.
+   * @param message A storage class for writing the message if present
+   * @return True if successful, false otherwise
+   */
+  boolean checkMessage(Message& message);
 
-}
+  /**
+   * Initializes the CANDriver so that it can read messages.
+   */
+  void begin();
+
+ private:
+  // Initialization fields
+  MCP_CAN _can;
+  uint8_t _pin;
+  uint8_t _alternate;
+  uint8_t _baudrate;
+
+  boolean shouldLog();
+};
+
 
 #endif
