1b0f7bd936c32aeab3fe550e1e9d4fc9b3808ed6
David Schwartz <davidphillipschwartz@gmail.com>
2017-06-07 23:31:29 -0700

    removed speed sensor code from CAN_Raven_Motor

--- CAN_Raven_Motor/CAN_Raven_Motor.ino
+++ CAN_Raven_Motor/CAN_Raven_Motor.ino
@@ -3,7 +3,6 @@
 #include <mcp_can.h>
 #include <SoftwareSerial.h>
 #include <SPI.h>
-
 #include <ubcsolar_can_ids.h>
 
 #define CAN_SS 9
@@ -21,13 +20,7 @@
 #define SEAT_SWITCH RELAY3PIN
 #define FS1_SWITCH RELAY4PIN
 
-#define SPEED_SENSOR_PIN A0
-#define SPEED_SENSOR_TIMEOUT 5000000 //microseconds
-#define SPEED_SENSOR_AVG_PERIOD 5000000.0 //microseconds, make sure to add .0 to the end to force floating point or it will fail
-#define SPEED_SENSOR_AVERAGE_MIN_NEW_VALUE_WEIGHT 500000 //microseconds
-
 #define DIR_SWITCH_RAMP_STEP_TIME_MS 10
-
 
 #define BUS_SPEED CAN_125KBPS
 
@@ -74,13 +67,6 @@ CAN_INIT:
     goto CAN_INIT;
   }
 
-  CAN.init_Mask(0, 0, 0x3ff); //mask 0 (for buffer 0) controls filters 0 and 1
-  CAN.init_Mask(1, 0, 0x3ff); //mask 1 (for buffer 1) controls filters 2 to 5
-
-  //to pass a filter, all bits in the msg id that are "masked" must be the same as in the filter.
-  //a message will pass if at least one of the filters pass it. 
-  CAN.init_Filt(0, 0, CAN_ID_MOTOR_CTRL);
-
 /* RHEO INIT */
   pinMode(RHEO_THROTTLE_SS,OUTPUT);
   pinMode(RHEO_REGEN_SS,OUTPUT);
@@ -116,25 +102,7 @@ CAN_INIT:
   Serial.println("fs1 on");
   digitalWrite(FS1_SWITCH, HIGH);
   delay(1000);
-
-/* SPEED SENSOR INIT */
-  pinMode(SPEED_SENSOR_PIN, INPUT);
-}
-
-void printBuf(uint32_t frame_id, byte *frame_data, byte length) {
-     
-  Serial.print("[Rx] ID: ");
-  Serial.print(frame_id,HEX);
-        
-  Serial.print(" Len:");
-  Serial.print(length,HEX);
-      
-  Serial.print(" Data:[");
-  for (int i=0;i<length;i++) {
-    if (i>0) Serial.print(" ");
-    Serial.print(frame_data[i],HEX);
-  }
-  Serial.println("]"); 
+  
 }
 
 void msgHandleMotorCtrl(uint32_t frame_id, byte *frame_data, byte length){
@@ -161,7 +129,6 @@ void msgHandler(uint32_t frame_id, byte *frame_data, byte length) {
      msgHandleBrake(frame_id, frame_data, length);
    }else{
      Serial.print("unknown msg ");
-     printBuf(frame_id, frame_data, length);
    }
 }
 
@@ -218,66 +185,6 @@ void motorCtrlRun(){
   }
 }
 
-/* this function detects falling edges */
-void speedSensorRun(){
-  static unsigned long lastTime=micros();
-  static unsigned long thisTime;
-  static unsigned long period;
-  static boolean lastState=0;
-  static boolean timedout=false;
-  static boolean firstTime=true;
-
-  thisTime = micros();
-  if(thisTime-lastTime > SPEED_SENSOR_TIMEOUT){
-    speedHz.f=0;
-    timedout=true;
-    lastTime=thisTime;
-    //enable these for automatic sending (good for CAN)
-    Serial.println(speedHz.f);
-    sendSpeedSensorPacket();
-  }else{
-    if(lastState){
-      if(!digitalRead(SPEED_SENSOR_PIN)){ //detect falling edge
-        if(timedout){
-          lastTime=thisTime;
-          timedout=false;
-          lastState=false;
-          firstTime=true;
-        }else{
-          period=thisTime-lastTime;
-          if(firstTime || period>SPEED_SENSOR_AVG_PERIOD){
-            speedHz.f=1000000.0/period; //instantaneous speed
-            firstTime=false;
-          }else{
-            unsigned long periodForAvg = period > SPEED_SENSOR_AVERAGE_MIN_NEW_VALUE_WEIGHT ? period : SPEED_SENSOR_AVERAGE_MIN_NEW_VALUE_WEIGHT;
-            speedHz.f=speedHz.f * (1-periodForAvg/SPEED_SENSOR_AVG_PERIOD) + (1000000.0/period) * (period/SPEED_SENSOR_AVG_PERIOD); //average speed readings to reduce fluctuations
-          }
-          lastTime=thisTime;
-          lastState=false;
-          //enable these for automatic sending (good for CAN)
-          Serial.println(speedHz.f);
-          sendSpeedSensorPacket();
-        }
-      }
-    }else{
-      lastState=digitalRead(SPEED_SENSOR_PIN);
-    }
-  }
-}
-
-void sendSpeedSensorPacket(){
-  uint32_t frame_id = CAN_ID_SPEED_SENSOR;
-  byte frame_data[8];
-  byte length = 4;
-  
-  frame_data[0] = speedHz.b[0];
-  frame_data[1] = speedHz.b[1];
-  frame_data[2] = speedHz.b[2];
-  frame_data[3] = speedHz.b[3];
-  
-  CAN.sendMsgBuf(frame_id, 0, length, frame_data);
-}
-
 void loop() {
   
   byte length;
@@ -296,7 +203,6 @@ void loop() {
     target_throttle = 0;
   }
   
-  speedSensorRun();
   motorCtrlRun();
 }
 
