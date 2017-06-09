3f46404308d9e5f42cb68847a7717760dbfea78f
David Schwartz <davidphillipschwartz@gmail.com>
2017-06-08 23:41:45 -0700

    steering wheel: fixed overflows in regen and throttle sensing

--- CAN_Raven_Steering_Wheel/CAN_Raven_Steering_Wheel.ino
+++ CAN_Raven_Steering_Wheel/CAN_Raven_Steering_Wheel.ino
@@ -7,7 +7,7 @@
 #define HORN_PIN 0
 #define LEFT_SIGNAL_PIN 1
 #define RIGHT_SIGNAL_PIN 2
-#define BRAKE_PIN 5
+#define REGEN_PIN 15
 #define THROTTLE_PIN 14
 #define DIRECTION_PIN 7
 
@@ -17,10 +17,10 @@
 #define BYTE_SIGNAL_STATUS 3
 
 #define THROTTLE_THRESHOLD 0
-#define THROTTLE_MULTIPLIER 1
+#define THROTTLE_MULTIPLIER 1 // should be <= 1
 
 #define REGEN_THRESHOLD 0
-#define REGEN_MULTIPLIER 1
+#define REGEN_MULTIPLIER 1 // should be <= 1
 
 #define HEARTBEAT_TIME 100 //in ms
 
@@ -42,6 +42,7 @@ void setup() {
   previousMillis = millis();
 
   pinMode(THROTTLE_PIN, INPUT);
+  pinMode(REGEN_PIN, INPUT);
   
   Serial.println(F("System initialized"));
 
@@ -66,7 +67,7 @@ void processSignals() {
   if(digitalRead(RIGHT_SIGNAL_PIN)){
     bitSet(txmsg.buf[BYTE_SIGNAL_STATUS],1);
   }
-  if(analogRead(BRAKE_PIN) > REGEN_THRESHOLD){
+  if(analogRead(REGEN_PIN) > REGEN_THRESHOLD){
     bitSet(txmsg.buf[BYTE_SIGNAL_STATUS],2);
   }
   if(digitalRead(HORN_PIN)){
@@ -74,19 +75,21 @@ void processSignals() {
   }
 }
 
-int processThrottle() {
+byte processThrottle() {
   int rawThrottle = analogRead(THROTTLE_PIN);
-  if(rawThrottle > THROTTLE_THRESHOLD) {
-    return THROTTLE_MULTIPLIER * rawThrottle;
+  byte byteThrottle = rawThrottle / 1023.0 * 255.0;
+  if(byteThrottle > THROTTLE_THRESHOLD) {
+    return THROTTLE_MULTIPLIER * byteThrottle;
   } else {
     return 0;
   }
 }
 
-int processRegen() {
-  int rawRegen = analogRead(BRAKE_PIN);
-  if(rawRegen > REGEN_THRESHOLD) {
-    return REGEN_MULTIPLIER * rawRegen;
+byte processRegen() {
+  int rawRegen = analogRead(REGEN_PIN);
+  byte byteRegen = rawRegen / 1023.0 * 255.0;
+  if(byteRegen > REGEN_THRESHOLD) {
+    return REGEN_MULTIPLIER * byteRegen;
   } else {
     return 0;
   }
